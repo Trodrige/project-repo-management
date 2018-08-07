1035c1926bb4e68a73f1f71481c624741bba485d
SFarida <faridagueleu@gmail.com>
2018-08-07 11:51:37 +0100

    add student module

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Project;
+use App\FileUpload;
+use File;
+use Storage;
+
 
 class ProjectController extends Controller
 {
@@ -45,7 +49,21 @@ class ProjectController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $doc = new Project;
+    	$doc->title = $request->title;
+        $doc->description = $request->description;
+        $doc->type = $request->type;
+        $doc->date_validated = $request->date_validated;
+        $doc->filename_pdf = FileUpload::savefile($request,'filename_pdf');
+        $doc->zip_filename = FileUpload::savezip($request,'zip_filename');
+        $doc->owner_id = $request->owner_id;
+        $doc->admin_id = $request->admin_id;
+    	if ($doc->save()) {
+            //return view('home');
+            return redirect()->route('home')
+                        ->with('success','Product created successfully.');
+            
+    	}
     }
 
     /**
